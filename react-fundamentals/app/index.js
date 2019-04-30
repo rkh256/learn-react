8ca87e1f871f6ee6ebdd84eeaec6d3d97ada7bdf
Randall Hughes <randall@predictspring.com>
2019-04-30 11:48:57 -0700

    initial commit

---
+++ react-fundamentals/app/index.js
@@ -0,0 +1,23 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+require('./index.css');
+
+// Things compoenents are responsible for
+// state
+// lifecycle events
+// UI
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
